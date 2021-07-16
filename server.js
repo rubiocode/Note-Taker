9630ca01830bcf2d3710677b3f6fc6cc36bdc628
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-07-15 22:49:26 -0700

    npm initiated and server.js code started

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+
+// Setting up express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Setting up express app to handle data parsing
+
+app.use(express.static(path.join(_dirname, './public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Getting express app routes 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//Starts the server to begin listening
+app.listen(PORT, ()=> console.log(`Server listening on http://localhost:${PORT}`));
