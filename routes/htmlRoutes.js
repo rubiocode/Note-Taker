7004b40f76457decb47ab0a3993a9cc72406aa10
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-07-15 23:11:43 -0700

    coded htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+
+// Module exports
+module.exports = function (app) {
+    app.use(express.static(path.join(__dirname, "../public")));
+
+//HTML GET Requests
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+};
